3e3055852d37ae33350d4277a954133ed42de449
Anjali Kevadiya <angeldungrani5@gmail.com>
2020-12-08 21:44:12 -0500

    created orm object and query data for view all the burgers

--- config/orm.js
+++ config/orm.js
@@ -38,3 +38,18 @@ function objToSql(ob) {
   // translate array of strings to a single comma-separated string
   return arr.toString();
 }
+
+// Object for all our SQL statement functions.
+var orm = {
+  all: function (tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+};
+// Export the orm object for the model (cat.js).
+module.exports = orm;
