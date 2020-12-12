a88f34c8ef64b27e508abf225460f7dc0b0a8bf8
Anjali Kevadiya <angeldungrani5@gmail.com>
2020-12-12 12:42:43 -0500

    change code in root route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,13 +7,7 @@ var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function (req, res) {
-  burger.all(function (data) {
-    var hbsObject = {
-      burgers: data,
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
-  });
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 router.post("/api/burgers", function (req, res) {
