31ca5ff0b7db5add3fd64b81bff41619c2e0c877
Anjali Kevadiya <angeldungrani5@gmail.com>
2020-12-12 13:32:22 -0500

    created devoured onclick to hit update burger endpoint using ajax call

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -45,6 +45,7 @@ $(function () {
     }
   });
 
+  // new burger on click
   $(".create-form").on("submit", function (event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
@@ -62,6 +63,28 @@ $(function () {
       contentType: "application/json",
     }).then(function () {
       console.log("created new burger");
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+
+  // devoured onclick to update value in db
+  $(document).on("click", ".change-devour", function (event) {
+    var id = $(this).data("id");
+    var newDevour = $(this).data("newdevour") === true;
+
+    var newDevourState = {
+      devoured: newDevour,
+    };
+
+    // Send the PUT request.
+    $.ajax("/burgers/" + id, {
+      type: "PUT",
+      data: JSON.stringify(newDevourState),
+      dataType: "json",
+      contentType: "application/json",
+    }).then(function () {
+      console.log("changed devour to", newDevour);
       // Reload the page to get the updated list
       location.reload();
     });
